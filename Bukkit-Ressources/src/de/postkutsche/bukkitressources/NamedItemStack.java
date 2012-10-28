4afa33f8e7ada83133e4f0557a46ee4305b5f97d
Leo Friedrichs <postcrafter@googlemail.com>
2012-10-28 09:38:03 +0100

    test

---
+++ Bukkit-Ressources/src/de/postkutsche/bukkitressources/NamedItemStack.java
@@ -0,0 +1,50 @@
+package de.postkutsche.testing;
+
+import net.minecraft.server.NBTTagCompound;
+
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+
+public class NamedItemStack {
+
+	private final ItemStack itemStack;
+	
+	public NamedItemStack(ItemStack itemStack) {
+		this.itemStack = itemStack;
+		CraftItemStack cis = ((CraftItemStack)this.itemStack);
+		NBTTagCompound tag = cis.getHandle().getTag();
+		if (tag == null) {
+			cis.getHandle().setTag(new NBTTagCompound());
+		}
+	}
+	
+	private boolean hasDisplay() {
+		return this.getDisplay() == null ? false : true;
+	}
+	
+	private NBTTagCompound getDisplay() {
+		return ((CraftItemStack)this.itemStack).getHandle().getTag().getCompound("display");
+	}
+	
+	private NBTTagCompound addDisplay() {
+		NBTTagCompound display = new NBTTagCompound();
+		((CraftItemStack)this.itemStack).getHandle().getTag().setCompound("display", display);
+		return display;
+	}
+	
+	public String getName() {
+		if (!hasDisplay()) {
+			return null;
+		}
+		System.out.println(getDisplay().get("Name"));
+		return getDisplay().getString("Name");
+	}
+	
+	public void setName(String name) {
+		NBTTagCompound display = this.getDisplay();
+		if (!hasDisplay()) {
+			display = this.addDisplay();
+		}
+		display.setString("Name", name);
+	}
+}
---
+++ Bukkit-Ressources/src/de/postkutsche/bukkitressources/Testing.java
@@ -0,0 +1,38 @@
+package de.postkutsche.testing;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Testing extends JavaPlugin {
+
+	public void onEnable() {
+		
+	}
+	
+	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
+		if (!(cs instanceof Player)) {
+			return false;
+		}
+		Player p = (Player) cs;
+		
+		NamedItemStack item = new NamedItemStack(p.getItemInHand());
+		
+		if (args.length == 1) {
+			item.setName(args[0]);
+		} else {
+			String name = item.getName();
+			if (name == null) {
+				p.sendMessage("Dieser Gegenstand hat keinen Namen");
+			} else {
+				p.sendMessage(name);
+			}
+		}
+		
+	//	p.sendMessage(((CraftItemStack)p.getItemInHand()).getHandle().getTag()..get("title").toString());
+	//	p.sendMessage(((CraftItemStack)p.getItemInHand()).getHandle().getTag().get("author").toString());
+		return false;
+	}
+	
+}
